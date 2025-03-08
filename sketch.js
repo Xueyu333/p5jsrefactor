de9f3eb7b110d74ff00e894936c376f81b46c973
Shane <xl5458@nyu.edu>
2025-03-08 14:17:57 -0500

    refactored code version01

--- sketch.js
+++ sketch.js
@@ -1,55 +1,72 @@
-let r=50;
-let x=25;
-let angle=0;
+// Configuration
+const GRID_SIZE = 6;
+const CELL_SIZE = 100;
+const ROTATION_SPEED = 0.05;
+const ARC_RADIUS = 50;
+const ARC_OFFSET = 25;
+
+// Colors
+let topLeftColor, bottomRightColor;
+let backColor1, backColor2;
+let angle = 0;
 
 function setup() {
   createCanvas(600, 600);
-   topLeftColor = color(255, 0, 0);
-   bottomRightColor = color(0, 0, 255); 
-   
-  backColor1 = color (0, 0, 255);
-  backColor2 = color (255, 0, 0);
-  
- // noLoop(); 
+  initializeColors();
+}
+
+function initializeColors() {
+  topLeftColor = color(255, 0, 0);
+  bottomRightColor = color(0, 0, 255);
+  backColor1 = color(0, 0, 255);
+  backColor2 = color(255, 0, 0);
 }
 
 function draw() {
-  
-   for (let y = 0; y < height; y++) {
-    let interFactor = y / height; 
+  drawGradientBackground();
+  drawFlowerGrid();
+}
+
+function drawGradientBackground() {
+  for (let y = 0; y < height; y++) {
+    let interFactor = y / height;
     let backgroundColor = lerpColor(backColor1, backColor2, interFactor);
-    stroke(backgroundColor);  
+    stroke(backgroundColor);
     fill(backgroundColor);
-    rect(0, y, width, 1);  
+    rect(0, y, width, 1);
   }
-  
-  for (let i = 0; i < 6; i++) { 
-    for (let j = 0; j < 6; j++) { 
-      let offsetX = i * 100; 
-      let offsetY = j * 100; 
-      
-        push();
-      translate(offsetX + 50, offsetY + 50);
-       rotate(angle);
-      
-      let interFactor = (i + j) / 10;
-      let currentColor = lerpColor(topLeftColor, bottomRightColor, interFactor);
-      
-  fill(currentColor);     
-  arc(0, -25,  r,  r, PI*3/2, PI/2, CHORD);//top arc
-  fill(currentColor); 
-  arc(25, 0,  r,  r, 0, PI, CHORD);// right arc
-  fill(currentColor); 
-  arc(0, 25,  r,  r, PI/2, PI*3/2, CHORD);//bottom arc
-  fill(currentColor); 
-  arc(-25, 0,  r,  r, PI, 0, CHORD);//left arc
-  pop();
+}
+
+function drawFlowerGrid() {
+  for (let i = 0; i < GRID_SIZE; i++) {
+    for (let j = 0; j < GRID_SIZE; j++) {
+      drawFlower(i, j);
     }
-  
+  }
 }
-  
+
+function drawFlower(i, j) {
+  const offsetX = i * CELL_SIZE + CELL_SIZE/2;
+  const offsetY = j * CELL_SIZE + CELL_SIZE/2;
+  const interFactor = (i + j) / 10;
+  const currentColor = lerpColor(topLeftColor, bottomRightColor, interFactor);
+
+  push();
+  translate(offsetX, offsetY);
+  rotate(angle);
+  drawFlowerPetals(currentColor);
+  pop();
 }
+
+function drawFlowerPetals(petalColor) {
+  fill(petalColor);
+  // Draw four arcs to create the flower pattern
+  arc(0, -ARC_OFFSET, ARC_RADIUS, ARC_RADIUS, PI*3/2, PI/2, CHORD);  // top
+  arc(ARC_OFFSET, 0, ARC_RADIUS, ARC_RADIUS, 0, PI, CHORD);          // right
+  arc(0, ARC_OFFSET, ARC_RADIUS, ARC_RADIUS, PI/2, PI*3/2, CHORD);   // bottom
+  arc(-ARC_OFFSET, 0, ARC_RADIUS, ARC_RADIUS, PI, 0, CHORD);         // left
+}
+
 function mouseMoved() {
-  angle += 0.05;
-  // redraw(); 
+  angle += ROTATION_SPEED;
 }
